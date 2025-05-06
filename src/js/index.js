ff8daa12a0dadff173a58078d1fc57f0a56ed9ae
satorutoji <ezdummyacc@gmail.com>
2025-05-06 21:44:49 +0500

    the sections Header and Start

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-import '../css/style.css'
+import '../css/normalize.scss';
+import '../css/action.scss';
+import '../css/style.css';
+import '../css/block/header.scss';
+import '../css/block/main/start/start.scss';
+import '../css/block/main/start/section.scss';
+
 const { sayPrivet } = require('./test');
 
 console.log('test');
