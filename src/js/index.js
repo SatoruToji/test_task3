16ecee7aac7825d130e21dc539326c7914fce5a3
satorutoji <ezdummyacc@gmail.com>
2025-05-07 19:13:34 +0500

    the end

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,14 @@ import '../css/style.css';
 import '../css/block/header.scss';
 import '../css/block/main/start/start.scss';
 import '../css/block/main/start/section.scss';
+import '../css/block/main/dashboard/dashboard.scss';
+import '../css/block/main/dashboard/object-explorer.scss';
+import '../css/block/main/dashboard/card.scss';
+import '../css/block/main/dashboard/card-section.scss';
+import '../css/block/main/dashboard/card-subsection.scss';
 
-const { sayPrivet } = require('./test');
+import myPng from '../image/dashboard/1.png';
 
-console.log('test');
-
-sayPrivet();
+const img = document.createElement('img');
+img.src = myPng;
+document.getElementById('image-switcher').appendChild(img);
